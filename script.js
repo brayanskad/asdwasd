6e8a7c0b6737e49a6adab7052dc2798081321534
brayanskad <marquesbrayan4002@gmail.com>
2026-05-12 15:39:38 -0500

    finalizado

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const campoBusca = document.getElementById('campoBusca');
 const resultadoArea = document.getElementById('resultado');
 const msgErro = document.getElementById('msgErro');
 
-function realizarBusca (){
-    const nome =campoBusca.value.toLowerCase().trim();
+function realizarBusca() {
+    const nome = campoBusca.value.toLowerCase().trim();
 
-    if (nome === ""){
-        alert("Por favor, digite um nome, seu verme!")
+    if (nome === "") {
+        alert("Por favor, digite um nome, seu beta!")
         return;
     }
 
@@ -17,17 +17,36 @@ function realizarBusca (){
         .then(response => {
             if (!response.ok) {
                 throw new Error('Pokémon não encontrado');
-            } 
+            }
             return response.json();
         })
         .then(data => {
             msgErro.classList.add('d-none');
+
+
+            document.getElementById('pokeNome').textContent = data.name;
+            document.getElementById('pokeId').textContent = data.id;
+            document.getElementById('pokeAltura').textContent = data.height;
+            document.getElementById('pokePeso').textContent = data.weight;
+            document.getElementById('pokeTipo').textContent = data.types[0].type.name;
+            document.getElementById('pokeImg').src = data.sprites.front_default;
+
+            resultadoArea.classList.remove('d-none');
+
         })
 
-        document.getElementById('pokeNome').textContent = data.name;
-        document.getElementById('pokeId').textContent = data.id;
-        document.getElementById('pokeAltura').textContent = data.height;
-        document.getElementById('pokePeso').textContent = data.weight;
-        document.getElementById('pokeTipo').textContent = data.types[0].type.name;
-        document.getElementById('pokeImg').src = data.sprites.front_default;
+        .catch(error => {
+            resultadoArea.classList.add('d-none');
+            msgErro.classList.remove('d-none');
+            console.error('Erro Na Busca', error);
+        });
 }
+btnBuscar.addEventListener('click', realizarBusca);
+
+campoBusca.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        realizarBusca();
+    }
+})
+
+//brayan da silva marques
